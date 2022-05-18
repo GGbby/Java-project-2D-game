7a6eaa40c9905a62807a4bd1567dc2778b8c5b77
Tenderflash <66397269+Tenderrflash@users.noreply.github.com>
2022-05-18 15:46:19 +0800

    part1

---
+++ Java-project-2D-game/Java game/src/GamePanel.java
@@ -0,0 +1,44 @@
+
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements Runnable{
+//screen setting
+
+    final int originalTileSize = 16;//16*16 tile pic??
+    final int scale = 3;//3 times
+
+    final int tileSize = originalTileSize * scale; //display (16*3)*(16*3)
+    final int maxScreenCol =16;
+    final int maxScreenRow =12;
+    final int screenWidth = tileSize * maxScreenCol;
+    final int screenHeight = tileSize * maxScreenRow;//screen size
+
+    Thread gameThread;//start time image
+
+
+    public GamePanel(){
+
+        this.setPreferredSize(new Dimension(screenWidth,screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+
+    }
+
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
+
+    }
+
+    //gameloop
+    public void  run(){
+
+    }
+
+
+
+
+
+}
---
+++ Java-project-2D-game/Java game/src/Main.java
@@ -0,0 +1,20 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("2D Adventure");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();//pack() 方法 (method) 用來將視窗中的各個元件，自動依版面配置作適當的排列。
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        
+    }
+}
