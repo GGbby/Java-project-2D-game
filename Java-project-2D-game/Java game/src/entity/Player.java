0ef293c3144905ba47bddded52ff4ed5b311242b
GGbby <brian1375@gmail.com>
2022-05-19 12:08:24 +0800

    Part 5 7:25

--- Java-project-2D-game/Java game/src/entity/Player.java
+++ Java-project-2D-game/Java game/src/entity/Player.java
@@ -10,19 +10,24 @@ public class Player extends Entity{
     
     GamePanel gp;
     main.KeyHandler keyH;
+    public final int screenX;
+    public final int screenY;
 
     public Player(GamePanel gp, main.KeyHandler keyH){
 
         this.gp = gp;
         this.keyH = keyH;
 
+        screenX = gp.maxScreenCol/2;
+        screenY = gp.maxScreenRow/2;
+
         setDefaultCloseOperation();
         getPlayerImage();
     }
 
     public void setDefaultCloseOperation(){
-        x = 100;
-        y = 100;
+        worldX = gp.tileSize * 23;
+        worldY = gp.tileSize * 21;
         speed = 4;
         direction = "down";
     }
@@ -45,16 +50,16 @@ public class Player extends Entity{
         if (keyH.upPressed == true || keyH.downPressed == true || keyH.leftPressed == true || keyH.rightPressed == true) {
             if (keyH.upPressed == true) {
                 direction="up";
-                y -= speed;
+                worldY -= speed;
             }else if(keyH.downPressed == true){
                 direction="down";
-                y += speed;
+                worldY += speed;
             }else if(keyH.leftPressed == true){
                 direction="left";
-                x -= speed;
+                worldX -= speed;
             }else if(keyH.rightPressed == true){
                 direction="right";
-                x += speed;
+                worldX += speed;
             }
     
             spriteCounter++;
@@ -111,6 +116,6 @@ public class Player extends Entity{
             }
                 break;
         }
-        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+        g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
     }
 }
